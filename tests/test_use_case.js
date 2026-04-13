73b0023ebfabb56163f06832d1c10cf26a6c94f7
Loukilo27 <loukilo207@gmail.com>
2026-04-13 15:21:50 +0200

    changement js -> php

--- tests/test_use_case.js
+++ tests/test_use_case.js
@@ -1,15 +1,16 @@
 import { Selector } from 'testcafe';
 
-fixture `Use Case Nom en Majuscule`
-    .page `http://localhost:8080/index.html`;
+fixture `Use Case PHP`
+    .page `http://localhost:8080/index.php`; 
 
 test('Saisir un nom et vérifier s\'il s\'affiche en majuscule', async t => {
     const nameInput = Selector('#nameInput');
     const saveButton = Selector('#saveBtn');
-    const resultHeader = Selector('#displayName');
+    const resultHeader = Selector('#displayName').with({ timeout: 5000 });
 
     await t
         .typeText(nameInput, 'jean-pierre')
         .click(saveButton)
+        .expect(resultHeader.exists).ok('La page result.php n’a pas affiché l’élément #displayName')
         .expect(resultHeader.innerText).eql('JEAN-PIERRE');
 });
